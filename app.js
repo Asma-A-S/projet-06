c18b58d2d8f2b86bb7ab8dc3fd4730f281ba3999
Asma-A-S <asma.benzerrouk@gmail.com>
2023-02-12 11:16:31 +0100

    finalisé

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+// importer le framework express
 const express = require('express');
+//importer la bibliothèque mongoose pour accéder à la BDD mongoDB
 const mongoose = require('mongoose');
+//importer le middleware cors pour gérer les requêtes Cross-Origin Resource Sharing (CORS).
 const cors = require('cors');
+//importer le middleware path fournit des utilitaires pour travailler avec les chemins des images.
 const path = require('path');
+// importer helmet pour renforcer la sécurité
+// de l'application en ajoutant divers en-têtes HTTP
 const helmet = require('helmet');
+// module pour charger les variables d'environnement à partir d'un fichier .env
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -15,9 +22,9 @@ mongoose.connect(process.env.MONGO_URL,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
+// appel de notre application express
 const app = express();
-app.use(cors());
+app.use(cors())
 app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}));
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images')))
